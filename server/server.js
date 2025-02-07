4173455886d88c2f7b7a2c68283849b3a893525a
AndyZ <ziyaoz@andrew.cmu.edu>
2025-02-07 18:03:44 -0500

    added more

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.get("/api", (req, res) => {
+    res.json({ message: 'Hello from server!' })
+})
+
+app.listen(8000, () => { console.log('Server running on http://localhost:8000') })
